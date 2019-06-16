2cf64d663e25d0fcb7b037254ae0394e2b298ff9
Alex <alexandre.josse@gadz.org>
2019-06-16 19:43:22 +0200

    Re-write characters routes to include async functions

--- api/routes/characters.js
+++ api/routes/characters.js
@@ -7,34 +7,87 @@ const fs = require('fs');
 // retrieve all characters
 router.get('/',
   (req, res) => {
-    // Fake database import
-    var database = JSON.parse(
-      fs.readFileSync(__dirname + '/characters.json')
-      .toString()
-    );
-    res.status(200)
-      .json(database);
-  })
+    fs.readFile(__dirname + '/characters.json', (err, data) => {
+      if (err) {
+        next(err)
+      } else {
+        res.status(200)
+          .json(JSON.parse(data));
+      }
 
+    });
+  });
+
+// GET Request
+// '/characters/:id'
+// retrieve a character
 router.get('/:id',
-(req,res)=> {
-  var database = JSON.parse(
-    fs.readFileSync(__dirname + '/characters.json')
-    .toString()
-  );
-  var id = req.params.id;
+  (req, res, next) => {
+    fs.readFile(__dirname + '/characters.json', (err, data) => {
+      if (err) {
+        next(error);
+      } else {
+        var id = req.params.id;
+        var charactersDB = JSON.parse(data);
+        if (Object.keys(charactersDB).indexOf(id.toString()) === -1) {
+          var error = new Error("no item with this id");
+          error.status = 404;
+          next(error);
+        } else {
+          res.status(200)
+            .json(charactersDB[id.toString()]);
+        }
+      }
+    });
+  });
 
-  if (Object.keys(database).indexOf(id) !== -1){
-    res.status(200)
-    .json({
-      id : database[id]
+// POST Request
+// '/characters/:id'
+// create a character
+router.post('/', (req, res, next) => {
+  console.log(req.body)
+  if (req.body.firstName && req.body.lastName) {
+    console.log("begin")
+    fs.readFile(__dirname + '/characters.json', (err, data) => {
+      if (err) {
+        console.log("not OK")
+        next(err);
+      } else {
+        console.log("OK")
+        var charactersDB = JSON.parse(data);
+        var idArray = Object.keys(charactersDB)
+          .map((id) => {
+            return Number(id);
+          });
+        var maxId = null;
+        idArray.forEach((id) => {
+          if (id > maxId) {
+            maxId = id;
+          }
+        })
+        var newId = maxId + 1;
+        charactersDB[newId] = {
+          "firstName": req.body.firstName,
+          "lastName": req.body.lastName
+        };
+
+        fs.writeFile(__dirname + '/characters.json', JSON.stringify(charactersDB), (err) => {
+          if (err) {
+            next(err);
+          } else {
+            res.status(200).json({
+              message: "character created",
+              [newId]: charactersDB[newId]
+            })
+          }
+        })
+      }
     })
   } else {
-res.status(400).json(
-  {
-    message:"no item with this id"
-  }
-)
+    res.status(422).json({
+      message: 'missing parameters'
+    })
   }
 })
+
 module.exports = router;
--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const charactersRoutes = require('./api/routes/characters');
 const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
+// Characters Routes
+app.use('/characters', charactersRoutes);
 
-app.use('/characters',charactersRoutes);
+// Main error
+app.use((req, res, next) => {
+  var error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+    .json({
+      error: {
+        message: error.message
+      }
+    });
+
+});
 
 module.exports = app;
