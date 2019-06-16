df40004d731a247902fe6dca6f4a5dab6174fc27
Alex <alexandre.josse@gadz.org>
2019-06-16 14:19:13 +0200

    Add a GET character request

--- api/routes/characters.js
+++ api/routes/characters.js
@@ -2,20 +2,39 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-// Fake database import
-var database = JSON.parse(
-  fs.readFileSync( __dirname + '/characters.json')
-  .toString()
-);
-
 // GET Request
 // '/characters'
 // retrieve all characters
 router.get('/',
-  (req,res,next)=>{
+  (req, res) => {
+    // Fake database import
+    var database = JSON.parse(
+      fs.readFileSync(__dirname + '/characters.json')
+      .toString()
+    );
+    res.status(200)
+      .json(database);
+  })
 
-res.status(200)
-.json(database);
+router.get('/:id',
+(req,res)=> {
+  var database = JSON.parse(
+    fs.readFileSync(__dirname + '/characters.json')
+    .toString()
+  );
+  var id = req.params.id;
+
+  if (Object.keys(database).indexOf(id) !== -1){
+    res.status(200)
+    .json({
+      id : database[id]
+    })
+  } else {
+res.status(400).json(
+  {
+    message:"no item with this id"
+  }
+)
+  }
 })
-
 module.exports = router;
